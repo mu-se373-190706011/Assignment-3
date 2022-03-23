8e638adbca96486dc741f2429d69f651e42190d8
mu-se373-190706011 <101144051+mu-se373-190706011@users.noreply.github.com>
2022-03-23 10:37:54 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,39 +1,107 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(myApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class myApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // createState uygulama
+    return MyAppState();
+  }
+}
 
+//Buton yapılandırması..
+
+class MyAppState extends State<myApp> {
+  var counter = 0;
+  var text = "Click :)";
+  void restartCounter() {
+    setState(() {
+      counter = 0;
+      textMessage();
+    });
+  }
+
+  void clickedCounter() {
+    setState(() {
+      counter += 1;
+      textMessage();
+    });
+  }
+
+  void textMessage() {
+    setState(() {
+      if (counter == 0) {
+        text = "Click :)";
+      } else {
+        text = "Click again..";
+      }
+    });
+  }
+
+// Buton ve yazılar için yapılandırmalar..
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: const MyHomePage(title: 'Assignment 3'),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Assignment 3"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.fromLTRB(5, 10, 8, 10),
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                  primary: Color.fromARGB(255, 247, 0, 0),
+                  minimumSize: Size(30, 1),
+                  side: BorderSide(
+                    width: 20.0,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  )),
+              child: Text("RESTART"),
+              onPressed: restartCounter,
+            ),
+          ),
+          Text(
+            "For restart please RESTART click!",
+            style: TextStyle(fontSize: 14),
+          ),
+          Padding(padding: EdgeInsets.all(100.0)),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.fromLTRB(10, 250, 10, 10),
+            child: ElevatedButton(
+              child: Text(text),
+              onPressed: clickedCounter,
+              style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 0, 0, 0),
+                  padding: const EdgeInsets.all(15),
+                  textStyle: TextStyle(fontSize: 22),
+                  alignment: Alignment.center),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
+            child: Text(
+              counter.toString(),
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+          Text(
+            text,
+            style: TextStyle(fontSize: 15),
+          )
+        ],
+      ),
+    ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
